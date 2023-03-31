57e2cc240f34d37aba1f98e7fe53cf9988dd19bd
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2023-03-31 17:45:19 +0530

    Fix typo

--- src/StateData.hx
+++ src/StateData.hx
@@ -15,12 +15,12 @@ class StateData {
 	}
 
 	public static function read(io:BytesReader) {
-		return new StateData(io.readInt32(), io.readInt32(), io.readInt32());
+		return new StateData(io.readInt32(), io.readInt32(), io.readInt16());
 	}
 
 	public function write(io:BytesWriter) {
 		io.writeInt32(this.surfaceIndex);
 		io.writeInt32(this.mapIndex);
-		io.writeInt32(this.lightStateIndex);
+		io.writeInt16(this.lightStateIndex);
 	}
 }
